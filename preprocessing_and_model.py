999c7f15c4e8dbc5aad42270e50aac35e5d9ff75
Janina Nuber <34737944+Janinanu@users.noreply.github.com>
2018-02-19 14:33:17 +0100

    Update, work in progress

--- preprocessing_and_model.py
+++ preprocessing_and_model.py
@@ -22,8 +22,13 @@ import keras
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from keras.layers import LSTM, Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam
+from keras import regularizers
+
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
+
 #%%
 
 def load_names_dict(data_directory):
@@ -150,20 +155,20 @@ def get_max_len():
 max_len = get_max_len()
 
 #%% 
-def name_to_tensor(name):
+def name_to_array(name):
     one_hot_name = to_categorical([alphabet.find(letter) for letter in name], num_classes=len(alphabet))
     num_pads = max_len-len(name)
     one_hot_name_padded = np.vstack((one_hot_name, np.zeros((num_pads,len(alphabet)))))
     return one_hot_name_padded
-
-
+  
+        
 #%%
 
-def create_name_vector_dict(names_dict): #names_train_dict, names_valid_dict, names_test_dict
-    names_vector_dict = {}
-    for language, name_list in names_dict.items():
+def create_name_array_dict(names_dict): #names_train_dict, names_valid_dict, names_test_dict
+    names_array_dict = {}
+    for language, name_list in sorted(names_dict.items()):
             for name in name_list:  
-                names_vector_dict[language] = [name_to_tensor(str(name)) for name in name_list]
+                names_array_dict[language] = [name_to_array(str(name)) for name in name_list]
    
 #    in_dict = str(names_dict)
 #    format_add = in_dict.split("_")[0]
@@ -171,24 +176,34 @@ def create_name_vector_dict(names_dict): #names_train_dict, names_valid_dict, na
 #    
 #    out_dict = names_vector_dict
 
-    return names_vector_dict
+    return names_array_dict
     #return name_vecs_%s_dict (% names_dict.split("_")[1])
 #%%
-name_vector_train_dict = create_name_vector_dict(names_train_dict) 
-name_vector_valid_dict = create_name_vector_dict(names_valid_dict) 
-name_vector_test_dict = create_name_vector_dict(names_test_dict) 
-    
+name_array_train_dict = create_name_array_dict(names_train_dict) 
+name_array_valid_dict = create_name_array_dict(names_valid_dict) 
+name_array_test_dict = create_name_array_dict(names_test_dict) 
+#%% 
+def array_to_name(name_array): #max_len x len(alphabet)
+    name = ""
+    for letter_array in name_array:
+        for position in letter_array:
+            if position == 1.0:
+                index = np.where(letter_array==position)
+                letter = alphabet[index[0][0]]
+                name += letter
+    return name 
+
 #%%
-def create_name_tensor_dict(names_vector_dict):
+def create_name_tensor_dict(names_array_dict):
     name_tensor_dict = {}
-    for language, vec_list in names_vector_dict.items():
+    for language, vec_list in sorted(names_array_dict.items()):
         name_tensor_dict[language] = np.concatenate(vec_list, axis = 0).reshape(-1, max_len, len(alphabet))
     return name_tensor_dict
 #%%
 
-name_tensor_train_dict = create_name_tensor_dict(name_vector_train_dict)
-name_tensor_valid_dict = create_name_tensor_dict(name_vector_valid_dict)
-name_tensor_test_dict = create_name_tensor_dict(name_vector_test_dict)
+name_tensor_train_dict = create_name_tensor_dict(name_array_train_dict)
+name_tensor_valid_dict = create_name_tensor_dict(name_array_valid_dict)
+name_tensor_test_dict = create_name_tensor_dict(name_array_test_dict)
 
 
 #%%
@@ -209,51 +224,129 @@ label_tensor_train_dict = create_label_tensor_dict(name_tensor_train_dict)
 label_tensor_valid_dict = create_label_tensor_dict(name_tensor_valid_dict)
 label_tensor_test_dict = create_label_tensor_dict(name_tensor_test_dict)
 #%%
+def create_data(name_tensor_dict):
+    #count_examples = 0
+    stack_data = np.ones((1, max_len, len(alphabet)))
+    for language, tensor in sorted(name_tensor_dict.items()):
+        stack_data = np.concatenate((stack_data, tensor), axis = 0)
+        #count_examples += tensor.shape[0]
+    data = stack_data[1:]
+    return data
+#%%
+train_data = create_data(name_tensor_train_dict)
+valid_data = create_data(name_tensor_valid_dict)
+test_data = create_data(name_tensor_test_dict)
+
+#%%
+def create_labels(label_tensor_dict):
+    num_classes = len(label_tensor_dict)
+    stack_labels = np.ones((1, num_classes))
+    for language, tensor in sorted(label_tensor_dict.items()):
+        stack_labels = np.concatenate((stack_labels, tensor), axis = 0)
+    labels = stack_labels[1:]
+    return labels
+#%%
+train_labels = create_labels(label_tensor_train_dict)
+valid_labels = create_labels(label_tensor_valid_dict)
+test_labels = create_labels(label_tensor_test_dict)
+
+#%%
+num_train_examples = train_data.shape[0]
+num_output_classes = train_labels.shape[1]
+#%%
 
 
+#GRID SEARCH: neurons, lr, dropout, kernel_regularizer, initializers, batch_size
+#
+#def create_model():
+#        model = Sequential()
+#        model.add(Dense(100, input_shape=(max_len, len(alphabet)), kernel_regularizer=regularizers.l2(0.001)))
+#        model.add(Dropout(0.85))
+#        model.add(LSTM(100, input_shape=(100,))) 
+#        model.add(Dropout(0.85))
+#        model.add(Dense(num_output_classes, activation='softmax'))
+#    
+#        model.compile(loss='categorical_crossentropy',
+#                  optimizer=adam,
+#                  metrics=['accuracy'])	
+#        
+#        return model
+#
+##%%
+#seed = 7
+#np.random.seed(seed)
+#
+#model = KerasClassifier(build_fn=create_model, epochs=1, verbose=0)
+#
+#batch_size = [100,200]
+#param_grid = dict(batch_size=batch_size)
+#grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+#grid_result = grid.fit(train_data, train_labels)
+#
+##history = grid.fit(train_data, train_labels, validation_data = (valid_data, valid_labels))
+#    
+##plt.plot(history.history['acc'])
+##plt.plot(history.history['val_acc'])
+##plt.title('model accuracy')
+##plt.ylabel('accuracy')
+##plt.xlabel('epoch')
+##plt.legend(['train', 'valid'], loc='upper left')
+##plt.show()
+### summarize history for loss
+##plt.plot(history.history['loss'])
+##plt.plot(history.history['val_loss'])
+##plt.title('model loss')
+##plt.ylabel('loss')
+##plt.xlabel('epoch')
+##plt.legend(['train', 'valid'], loc='upper left')
+##plt.show()
+#
+#
+#print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+#means = grid_result.cv_results_['mean_test_score']
+#stds = grid_result.cv_results_['std_test_score']
+#params = grid_result.cv_results_['params']
+#for mean, stdev, param in zip(means, stds, params):
+#    print("%f (%f) with: %r" % (mean, stdev, param))
+#%%+
+model = Sequential()
+
+model.add(Dense(200, input_shape=(max_len, len(alphabet)), kernel_regularizer=regularizers.l2(0.0001)))
+
+model.add(Dropout(0.85))
+
+model.add(LSTM(200, input_shape=(200,))) 
+
+model.add(Dropout(0.8))
+
+model.add(Dense(num_output_classes, activation='softmax'))
+
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, decay=1e-6)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+
+history = model.fit(train_data, train_labels,
+          epochs=2000,
+          batch_size=100,
+          validation_data = (valid_data, valid_labels))
+
+print(history.history.keys())
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
 
 
-
-#
-#data = tf.placeholder(tf.float32, [1, len(name_tensor), len(alphabet)]) #Batch Size, Sequence Length, Input Dimension
-#target = tf.placeholder(tf.float32, [None, len(lang_names_dict)])
-##%%
-#num_hidden = 10
-#cell = tf.nn.rnn_cell.LSTMCell(num_hidden,state_is_tuple=True)
-#val, state = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32)
-##%%
-#val = tf.transpose(val, [1, 0, 2])
-#last = tf.gather(val, int(val.get_shape()[0]) - 1)
-#
-#weight = tf.Variable(tf.truncated_normal([num_hidden, int(target.get_shape()[1])]))
-#bias = tf.Variable(tf.constant(0.1, shape=[target.get_shape()[1]]))
-##%%
-#
-#prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
-#
-#cross_entropy = -tf.reduce_sum(target * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
-#
-##%%
-#optimizer = tf.train.AdamOptimizer()
-#minimize = optimizer.minimize(cross_entropy)
-##%%
-#init_op = tf.global_variables_initializer()
-#sess = tf.Session()
-#sess.run(init_op)
-##%%
-#batch_size = 1
-#no_of_batches = int(len(data)/batch_size)
-#epoch = 5000
-#for i in range(epoch):
-#    ptr = 0
-#    for j in range(no_of_batches):
-#        inp, out = data[ptr:ptr+batch_size], train_output[ptr:ptr+batch_size]
-#        ptr+=batch_size
-#        sess.run(minimize,{data: inp, target: out})
-#    print("Epoch - ",str(i))
-#incorrect = sess.run(error,{data: test_input, target: test_output})
-#print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
-#sess.close()
-#
-#
-##
+#score = model.evaluate(test_data, test_labels, batch_size=128)
